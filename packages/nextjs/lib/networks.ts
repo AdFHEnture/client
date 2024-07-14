5a5802ee22610120b1c707f34f48eb8a89a01fb5
Mehmet Gürcan <78200658+MGurcan@users.noreply.github.com>
2024-07-14 07:55:54 +0200

    Zirrcuit added to evmnetworks

--- packages/nextjs/lib/networks.ts
+++ packages/nextjs/lib/networks.ts
@@ -13,6 +13,21 @@ export const customEvmNetworks = [
     networkId: 8008135,
   },
   {
+    blockExplorerUrls: ["https://explorer.zircuit.com"],
+    chainId: 48899,
+    name: "Zircuit",
+    rpcUrls: ["https://zircuit1.p2pify.com/"],
+    iconUrls: [
+      "https://media.licdn.com/dms/image/D560BAQFeVazkcWupCA/company-logo_200_200/0/1701799824387?e=2147483647&v=beta&t=9Hdhj2z8ezaDlxRlLBwXH2VTZvouTq8CMAAclgJjMiY",
+    ],
+    nativeCurrency: {
+      name: "Ethereum",
+      symbol: "ETH",
+      decimals: 18,
+    },
+    networkId: 48899,
+  },
+  {
     blockExplorerUrls: ["https://explorer.zero.network"],
     chainId: 4457845,
     name: "ZERϴ",
